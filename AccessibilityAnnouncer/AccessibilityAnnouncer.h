0aa5d3a89ab730116b0c6fdb284a0df226cc8d24
Sommer Panage <sommer@panage.org>
2015-09-11 19:07:38 -0700

    Initial commit

---
+++ AccessibilityAnnouncer/AccessibilityAnnouncer.h
@@ -0,0 +1,19 @@
+//
+//  AccessibilityAnnouncer.h
+//  AccessibilityAnnouncer
+//
+//  Created by Sommer Panage on 9/11/15.
+//  Copyright © 2015 Sommer Panage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AccessibilityAnnouncer.
+FOUNDATION_EXPORT double AccessibilityAnnouncerVersionNumber;
+
+//! Project version string for AccessibilityAnnouncer.
+FOUNDATION_EXPORT const unsigned char AccessibilityAnnouncerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AccessibilityAnnouncer/PublicHeader.h>
+
+
