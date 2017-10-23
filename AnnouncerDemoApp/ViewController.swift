20ab34cbbce57adbded36d7cd80c8ad70fe6e0db
Sommer Panage <spanage@Sommers-MacBook-Pro.local>
2017-10-23 15:14:40 -0700

    app updates

--- AnnouncerDemoApp/ViewController.swift
+++ AnnouncerDemoApp/ViewController.swift
@@ -25,22 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func didTapShortAnnouncement(sender: UIButton) {
-        dispatch_after(delay, dispatch_get_main_queue()) {
+    @IBAction func didTapShortAnnouncement(_ sender: Any) {
+        DispatchQueue.main.asyncAfter(deadline: deadline) {
             self.announcer.announce("Short.")
         }
     }
     
-    @IBAction func didTapLongAnnouncement(sender: UIButton) {
-        dispatch_after(delay, dispatch_get_main_queue()) {
+    @IBAction func didTapLongAnnouncement(_ sender: Any) {
+        DispatchQueue.main.asyncAfter(deadline: deadline) {
             self.announcer.announce("This is a long announcement with a lot to say.")
         }
     }
     
-    private var delay: dispatch_time_t {
+    private var deadline: DispatchTime {
         let index = delayControl.selectedSegmentIndex
-        let stringValue = delayControl.titleForSegmentAtIndex(index)!
-        return dispatch_time(DISPATCH_TIME_NOW, Int64(Double(stringValue)! * Double(NSEC_PER_SEC)))
+        let stringValue = delayControl.titleForSegment(at: index)!
+        return .now() + .seconds(Int(stringValue)!)
     }
 }
 
